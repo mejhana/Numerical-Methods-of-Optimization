5aab5e28c1c79b41001d40433735db0d5cdb7436
Meghana Rao <meghanarao.99@gmail.com>
2020-09-14 10:23:04 +0530

    region elimination methods

---
+++ Direct Search method/Region Elimination Methods/fibonacci.m
@@ -0,0 +1,38 @@
+min = str2double(input('enter lower bound' ,"s"))
+max = str2double(input('enter upper bound' ,"s"))
+m = str2double(input('enter number of evaluations' ,"s"))
+k = 2
+
+while(k < m )
+    lk = fibonacci(m - k + 1)/fibonacci(m+1)
+    x1 = min + lk
+    x2 = max - lk
+    f1 = eval(x1)
+    f2 = eval(x2)
+    
+   if (f1<f2)
+       max = x2
+    
+   elseif (f1>f2)
+       min = x1
+       
+   else 
+       min = x1
+       max = x2
+     
+   end
+   k= k+1 
+
+end
+      
+if (k == m )
+    disp('min found in interval')
+    min
+    max
+end
+
+
+%%
+function e = eval(x)
+    e = x^2 + 54/x ;
+end
